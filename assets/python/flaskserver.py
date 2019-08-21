820b740b4df3108afdcbcbf2f47868b9b4aae9b2
Niqui O'Neill <doneill@ncsu.edu>
2019-08-21 13:04:00 -0400

    update flask run

--- assets/python/flaskserver.py
+++ assets/python/flaskserver.py
@@ -205,4 +205,4 @@ def get_search(anno, filename):
         writetogithub(annodata_filename, annodata_yaml, True)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run()
